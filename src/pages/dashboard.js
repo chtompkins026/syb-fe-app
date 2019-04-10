707d5b1b82d8504f4fb1f1d874690d7f703fee98
chtompkins026 <chtompkins026@gmail.com>
2019-04-10 15:45:38 -0400

    Initial draft of dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,16 +3,59 @@ import { axiosInstance } from "../services";
 import { DateTime } from "luxon";
 
 export default function Dashboard({ history }) {
+  const [client, setClient] = useState({
+    bookings: []
+  });
+
+  useEffect(() => {
+    axiosInstance
+      .get(`/api/clients/1`) //TODO: the ID is hardcoded
+      .then(res => {
+        setClient(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }, []);
+
+  const { first_name, last_name, bookings } = client;
+
   return (
     <div>
       <button
         onClick={() => {
-          auth.logout();
-          history.push("/login");
+          axiosInstance
+            .post(`/oauth/revoke`)
+            .then(res => {
+              localStorage.removeItem("access_token");
+              localStorage.removeItem("refresh_token");
+              history.push("/login");
+            })
+            .catch(err => {
+              console.error(err);
+            });
         }}
       >
         Log out
       </button>
+      <div>
+        <div>{first_name}</div>
+        <div>{last_name}</div>
+        <ul>
+          {bookings.map(({ id, start, cost, status }) => (
+            <li key={id}>
+              <div>
+                {DateTime.fromISO(start).toLocaleString(
+                  DateTime.DATETIME_SHORT
+                )}
+              </div>
+              <div>COST: {cost}</div>
+              <div>{status}</div>
+              <button> UPDATE </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
