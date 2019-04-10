6d99bc1160c5f10d1ce438dbfb868dcbab1b2423
chtompkins026 <chtompkins026@gmail.com>
2019-04-10 17:19:11 -0400

    Simple logic added to List Search so one can search for instructors etc.

--- src/components/ListSearch/ListSearch.js
+++ src/components/ListSearch/ListSearch.js
@@ -5,5 +5,21 @@ export default class ListSearch extends Component {
     inputValue:""
   };
 
+  render(){
+    const { inputValue } = this.state;
+    const { updateSeach } = this.props;
 
+    return (
+      <div>
+        <input
+          value={inputValue}
+          onChange={e => {
+            const inputValue = e.target.value;
+            this.setState({ inputValue });
+            updateSeach(inputValue);
+          }}
+          />
+      </div>
+    );
+  }
 }
