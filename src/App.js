cd2f236ee143c08ee35c06b347f878bcc221445a
chtompkins026 <chtompkins026@gmail.com>
2019-04-10 14:38:22 -0400

    Add router to App.js

--- src/App.js
+++ src/App.js
@@ -5,24 +5,33 @@ import "./App.css";
 
 
 
+const PrivateRoute = ({ component: Component, ...props }) => {
+  //TODO: Add authentication logic here
+  return auth.isAuthenticated() ? (
+    <Route {...props} component={Component} />
+  ) : (
+    <Redirect to="/login" />
+  );
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <>
+            <NavBar />
+            <Layout>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route exact path="/instructors" component={Instructors} />
+              <Route path="/instructors/:slug" component={InstructorProfile} />
+              <PrivateRoute path="/dashboard" component={Dashboard} />
+              <Route path="/login" component={Login} />
+              <Route path="/signup" component={Signup} />
+            </Layout>
+          </>
+        </Router>
       </div>
     );
   }
