d1ee5086c0c3efaf0446db211c93a084c3e92f37
chtompkins026 <chtompkins026@gmail.com>
2019-04-10 15:50:51 -0400

    Initial commit for Instructors page

---
+++ src/pages/instructor.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom"
+
+import ListFilter from "../components/ListFilter/ListFilter";
+import ListSearch from "../components/ListSearch/ListSearch";
+
+export default class Instructors extends Component {
+  
+
+}
