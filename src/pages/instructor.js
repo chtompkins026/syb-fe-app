4d56ed74e2387398b5d93775d524b8c16cde13b2
chtompkins026 <chtompkins026@gmail.com>
2019-04-10 16:00:21 -0400

    Add in dymnamc filtering for instructors

--- src/pages/instructor.js
+++ src/pages/instructor.js
@@ -49,5 +49,60 @@ export default class Instructors extends Component {
       searchTerm
     } = this.state;
 
+    let computedInstructors = instructors;
 
-}
+        if (regionFilter) {
+          computedInstructors = computedInstructors.filter(
+            ({ region }) => region === regionFilter
+          );
+        }
+
+        let component;
+
+        if (isLoading) {
+          component = <div> loading data... </div>;
+        }
+
+        if (hasError) {
+          component = <div> there was an error </div>;
+        }
+
+        const searchedName = searchTerm.trim().toLowerCase();
+        computedInstructors = computedInstructors.filter(({ name }) => {
+          return name.toLowerCase().includes(searchedName);
+        });
+
+        if (!isLoading && !hasError) {
+          component = computedInstructors.map(
+            ({ name, age, bio, region, slug }) => (
+              <Link
+                to={{
+                  pathname: `/instructors/${slug}`,
+                  state: {
+                    name,
+                    age,
+                    bio,
+                    region
+                  }
+                }}
+                key={name}
+              >
+                <div style={{ border: "1px solid black" }}>
+                  <div>{name}</div>
+                  <div>{region}</div>
+                  <br />
+                </div>
+              </Link>
+            )
+          );
+        }
+
+        return (
+          <div>
+            <ListFilter options={regions} setFilter={this.setFilter} />
+            <ListSearch updateSearch={this.updateSearch} />
+            <div>{component}</div>
+          </div>
+        );
+      }
+    }
