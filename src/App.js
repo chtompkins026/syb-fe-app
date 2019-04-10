ecbb79918ba76914453509fca617a7dce59733ad
chtompkins026 <chtompkins026@gmail.com>
2019-04-10 16:01:28 -0400

    Add route, redirect, and switch to router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 
 import "./App.css";
 
