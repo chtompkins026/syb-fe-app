662efd3a8ff8b41287fbfbf8bfbf8088aea0988d
chtompkins026 <chtompkins026@gmail.com>
2019-04-10 15:04:56 -0400

    Create about page - first draft

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,42 @@
-import { Component } from "react"
+import React from "react"
 
-  class About extends Component {
+  class About extends React.Component {
     render () {
-      return <p> Hello! </p> 
+      return(
+        <React.Fragment>
+          <h1> Surf Yoga Beer </h1>
+            <h2> Who We Are </h2>
+              <p> Here at SurfYogaBeer, we create fitness adventures worldwide that
+                connect people from all over the world through a mutual love of surf yoga,
+                and our personal favorite: beer. We get our sweat in with workouts
+                designed to take advantage of the environments that we explore.
+                The nights following are filled with stories your future grandkids
+                will get sick of hearing, new friends that bring out your most
+                adventurous self, and maybe the one-beer-too-many that has you
+                untagging yourself from photos the next day. SYB adventures offer
+                workouts that will have you coming home looking fine as ever,
+                experiences that you never imagined possible, and a community of
+                people that love sweat-filled days and beer-filled nights just
+                as much as you do. </p>
+            <h2> What We Stand For </h2>
+              <p> We are a company that is community-oriented, creating family
+              in every paradise we venture to. SYB strives to create an environment
+               that is meditative, open-minded, and bold. We focus on a balanced
+              lifestyle of workin’ the bod during the day and twerkin’ the bod during
+              the night. Surf allows us to appreciate this beautiful world of ours,
+              yoga helps us remain present and thoughtful, and beer... because you know…
+              it’s not the bottled water making us drop it low. </p>
+
+              <p> Our adventures at SYB follow a simple mission: We detox to retox.
+              This means we put value in bettering our bodies and minds while also
+              enjoying the pleasures of life (beer). We go on excursions worldwide
+              to escape the rat-race while working out, experiencing cultures different
+              from our own, and making friendships with individuals of the same
+              venturesome spirit. We work out to push ourselves past the barriers
+              we build in our minds, we explore to appreciate the boundless experience
+              of other cultures, and we drink beer to celebrate this vibrant life of ours. </p>
+        </React.Fragment>
+      );
     }
   }
 
