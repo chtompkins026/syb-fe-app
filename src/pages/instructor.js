8901f11f63590fde2f13aa0b401871c95c483b43
chtompkins026 <chtompkins026@gmail.com>
2019-04-10 15:56:45 -0400

    Create axios call to get instructors from rails backend api

--- src/pages/instructor.js
+++ src/pages/instructor.js
@@ -6,6 +6,48 @@ import ListFilter from "../components/ListFilter/ListFilter";
 import ListSearch from "../components/ListSearch/ListSearch";
 
 export default class Instructors extends Component {
-  
+  state = {
+    regions: ["New York", "San Diego", "Miami"],
+    instructors: [],
+    isLoading: false,
+    hasError: false,
+    regionFilter: null,
+    searchTerm: ""
+  };
+
+  componentDidMount(){
+    this.setState({ isLoading: true });
+    axios
+      .get(`/api/instructors`)
+      .then(res => {
+        this.setState({
+          instructors: res.data.instructors,
+          isLoading: false,
+          hasError: false
+        });
+      })
+      .catch(err => {
+        this.setState({ isLoading: false, hasError: true });
+      });
+  }
+
+    setFilter = filter => {
+      this.setState({ regionFilter: filter });
+    }
+
+    updateSearch = searchTerm => {
+    this.setState({ searchTerm });
+  };
+
+  render() {
+    const {
+      instructors,
+      isLoading,
+      hasError,
+      regionFilter,
+      regions,
+      searchTerm
+    } = this.state;
+
 
 }
