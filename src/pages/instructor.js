dcf008d06de74a47e2e7214e17b993b19439778b
chtompkins026 <chtompkins026@gmail.com>
2019-04-10 17:32:38 -0400

    Little bug edit in Instructors page

--- src/pages/instructor.js
+++ src/pages/instructor.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 import ListFilter from "../components/ListFilter/ListFilter";
 import ListSearch from "../components/ListSearch/ListSearch";
 
 export default class Instructors extends Component {
   state = {
-    regions: ["New York", "San Diego", "Miami"],
+    regions: ["New York", "New Jersey", "Boston"],
     instructors: [],
     isLoading: false,
     hasError: false,
@@ -15,10 +15,10 @@ export default class Instructors extends Component {
     searchTerm: ""
   };
 
-  componentDidMount(){
+  componentDidMount() {
     this.setState({ isLoading: true });
     axios
-      .get(`/api/instructors`)
+      .get("data.json")
       .then(res => {
         this.setState({
           instructors: res.data.instructors,
@@ -31,11 +31,11 @@ export default class Instructors extends Component {
       });
   }
 
-    setFilter = filter => {
-      this.setState({ regionFilter: filter });
-    }
+  setFilter = filter => {
+    this.setState({ regionFilter: filter });
+  };
 
-    updateSearch = searchTerm => {
+  updateSearch = searchTerm => {
     this.setState({ searchTerm });
   };
 
@@ -51,58 +51,58 @@ export default class Instructors extends Component {
 
     let computedInstructors = instructors;
 
-        if (regionFilter) {
-          computedInstructors = computedInstructors.filter(
-            ({ region }) => region === regionFilter
-          );
-        }
-
-        let component;
-
-        if (isLoading) {
-          component = <div> loading data... </div>;
-        }
-
-        if (hasError) {
-          component = <div> there was an error </div>;
-        }
-
-        const searchedName = searchTerm.trim().toLowerCase();
-        computedInstructors = computedInstructors.filter(({ name }) => {
-          return name.toLowerCase().includes(searchedName);
-        });
-
-        if (!isLoading && !hasError) {
-          component = computedInstructors.map(
-            ({ name, age, bio, region, slug }) => (
-              <Link
-                to={{
-                  pathname: `/instructors/${slug}`,
-                  state: {
-                    name,
-                    age,
-                    bio,
-                    region
-                  }
-                }}
-                key={name}
-              >
-                <div style={{ border: "1px solid black" }}>
-                  <div>{name}</div>
-                  <div>{region}</div>
-                  <br />
-                </div>
-              </Link>
-            )
-          );
-        }
-
-        return (
-          <div>
-            <ListFilter options={regions} setFilter={this.setFilter} />
-            <ListSearch updateSearch={this.updateSearch} />
-            <div>{component}</div>
-          </div>
-        );
-      }
+    if (regionFilter) {
+      computedInstructors = computedInstructors.filter(
+        ({ region }) => region === regionFilter
+      );
     }
+
+    let component;
+
+    if (isLoading) {
+      component = <div> loading data... </div>;
+    }
+
+    if (hasError) {
+      component = <div> there was an error </div>;
+    }
+
+    const searchedName = searchTerm.trim().toLowerCase();
+    computedInstructors = computedInstructors.filter(({ name }) => {
+      return name.toLowerCase().includes(searchedName);
+    });
+
+    if (!isLoading && !hasError) {
+      component = computedInstructors.map(
+        ({ name, age, bio, region, slug }) => (
+          <Link
+            to={{
+              pathname: `/instructors/${slug}`,
+              state: {
+                name,
+                age,
+                bio,
+                region
+              }
+            }}
+            key={name}
+          >
+            <div style={{ border: "1px solid black" }}>
+              <div>{name}</div>
+              <div>{region}</div>
+              <br />
+            </div>
+          </Link>
+        )
+      );
+    }
+
+    return (
+      <div>
+        <ListFilter options={regions} setFilter={this.setFilter} />
+        <ListSearch updateSearch={this.updateSearch} />
+        <div>{component}</div>
+      </div>
+    );
+  }
+}
