9181b21d93294a6a6eae37c41f1fa3d4b4d0685a
chtompkins026 <chtompkins026@gmail.com>
2019-04-10 17:22:59 -0400

    Initial iteration of NavBar added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,7 +4,24 @@ import { Link } from "react-router-dom";
 import "./NavBar.css";
 
 const NavBar = () => {
+  return (
+    <div className="navbar">
+      <li className="navbar-menu">
+        <ul className="navbar-link">
+          <Link to="/home"> Home </Link>
+        </ul>
+        <ul className="navbar-link">
+          <Link to="/instructors"> instructors </Link>
+        </ul>
+        <ul className="navbar-link">
+          <Link to="/login"> LOGIN </Link>
+        </ul>
+        <ul className="navbar-link">
+          <Link to="/signup"> SIGN UP </Link>
+        </ul>
+      </li>
+    </div>
+  );
+};
 
-}
-
-export default NavBar; 
+export default NavBar;
