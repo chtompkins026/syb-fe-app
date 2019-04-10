19a4f16390f5b3d2efca3ece137c2e8b3e8ef98d
chtompkins026 <chtompkins026@gmail.com>
2019-04-10 17:47:28 -0400

    Fix to signup page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,80 @@
 import React, { useState } from "react";
 import { axiosInstance } from "../services";
-
+import { Input } from "../components/Input/Input" 
 
 export default function Signup({history}) {
-  
+  export default function Signup({ history }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [errors, setErrors] = useState({ password: [] });
+
+  const validateForm = () => {
+    let errors = { password: [] };
+
+    if (password < 8) {
+      errors.password.push("Password must be 8 characters");
+    }
+    if (password.toLowerCase() === password) {
+      errors.password.push("Password must have at least 1 uppercase character");
+    }
+
+    return errors;
+  };
+
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        const errors = validateForm();
+        if (errors.password.length > 0) {
+          setErrors(errors);
+          return;
+        }
+
+        axiosInstance
+          .post("/api/users", {
+            name,
+            email,
+            password,
+            account_type: "1"
+          })
+          .then(res => {
+            history.push("/profile", { id: res.data.id });
+          })
+          .catch(err => {
+            //TODO: handle error and provide error message and clear form
+            console.error(err);
+          });
+      }}
+    >
+      <h2>Sign up</h2>
+      <Input
+        errors={[]}
+        type="text"
+        placeholder="name"
+        name="name"
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <Input
+        errors={[]}
+        type="text"
+        placeholder="email"
+        name="email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <Input
+        errors={errors.password}
+        type="password"
+        placeholder="password"
+        name="password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <button type="submit"> SUBMIT </button>
+    </form>
+  );
 }
