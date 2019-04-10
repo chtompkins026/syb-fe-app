581171a71aee540db80d804afac9cde484709655
chtompkins026 <chtompkins026@gmail.com>
2019-04-10 17:59:45 -0400

    Add in profile to navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -11,10 +11,16 @@ const NavBar = () => {
           <Link to="/home"> Home </Link>
         </ul>
         <ul className="navbar-link">
+          <Link to="/about"> About </Link>
+        </ul>
+        <ul className="navbar-link">
           <Link to="/workouts"> Workouts </Link>
         </ul>
         <ul className="navbar-link">
           <Link to="/instructors"> Instructors </Link>
+        </ul>
+        <ul className="navbar-link">
+          <Link to="/profile"> Profile </Link>
         </ul>
         <ul className="navbar-link">
           <Link to="/login"> LOGIN </Link>
