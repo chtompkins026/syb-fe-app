037211198b9ad736893bcdd39a84b744d57375cb
chtompkins026 <chtompkins026@gmail.com>
2019-04-11 17:38:00 -0400

    Add in Navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,27 +6,28 @@ import "./NavBar.css";
 const NavBar = () => {
   return (
     <div className="navbar">
+        <img className="nav-image" src={"favicon.png"} alt="SYB Logo" />
       <li className="navbar-menu">
-        <ul className="navbar-link">
-          <Link to="/home"> Home </Link>
+        <ul className="navbar-link 1">
+          <Link to="/" className="navlinks"> Home </Link>
         </ul>
-        <ul className="navbar-link">
-          <Link to="/about"> About </Link>
+        <ul className="navbar-link 2">
+          <Link to="/about" className="navlinks"> About </Link>
         </ul>
-        <ul className="navbar-link">
-          <Link to="/workouts"> Workouts </Link>
+        <ul className="navbar-link 3">
+          <Link to="/workouts" className="navlinks"> Workouts </Link>
         </ul>
-        <ul className="navbar-link">
-          <Link to="/instructors"> Instructors </Link>
+        <ul className="navbar-link 4">
+          <Link to="/instructors" className="navlinks"> Instructors </Link>
         </ul>
-        <ul className="navbar-link">
-          <Link to="/profile"> Profile </Link>
+        <ul className="navbar-link 5">
+          <Link to="/profile" className="navlinks"> Profile </Link>
         </ul>
-        <ul className="navbar-link">
-          <Link to="/login"> LOGIN </Link>
+        <ul className="navbar-link 6">
+          <Link to="/login" className="navlinks"> Login </Link>
         </ul>
-        <ul className="navbar-link">
-          <Link to="/signup"> SIGN UP </Link>
+        <ul className="navbar-link 7">
+          <Link to="/signup" className="navlinks"> Sign Up </Link>
         </ul>
       </li>
     </div>
