65a71cc9b0241cd25a177185132b264c316708e1
Michal Czaplinski <mmczaplinski@gmail.com>
2019-04-17 10:16:23 -0500

    add the redux store persistence

--- src/App.js
+++ src/App.js
@@ -54,6 +54,31 @@ const store = createStore(rootReducer, {}, composeEnhancers(
     applyMiddleware(thunk)
 ));
 
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("appState");
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+export const saveState = state => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("appState", serializedState);
+  } catch {
+    // ignore write errors
+  }
+};
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
 const App = () => (
   <div className="App">
     <Router>
