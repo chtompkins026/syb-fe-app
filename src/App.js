9dd0abe96e22adf0077043e807128b33b2d234d3
chtompkins026 <chtompkins026@gmail.com>
2019-04-18 17:12:27 -0400

    Redux thunk

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import {
   Redirect,
   Switch
 } from "react-router-dom";
-import { createStore, applyMiddleware } from "redux";
-import thunk from 'redux-thunk'; 
-import { Provider } from "react-redux";
+
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import "./App.css";
 
@@ -38,22 +39,23 @@ const PrivateRoute = ({ component: Component, ...props }) => {
   );
 };
 
-// function reducer(state = {}, action) {
-//   switch (action.type) {
-//     case "SET_ACCESS_TOKEN":
-//       return {
-//         ...state,
-//         userId: action.accessToken
-//       };
-//     default:
-//       return state;
-//   }
-// }
+const logger = store => {
+  return next => {
+      return action => {
+          console.log('[Middleware] Dispatching', action);
+          const result = next(action);
+          console.log('[Middleware] next state', store.getState());
+          return result;
+      }
+  }
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+   reducer,
+   composeEnhancers(applyMiddleware(logger, thunk))
+   );
 
 const App = () => (
   <div className="App">
