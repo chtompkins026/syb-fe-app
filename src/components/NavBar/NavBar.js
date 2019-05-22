18b8c6242e91b849c0bf133656b65d75dbbb328f
Michal Czaplinski <mmczaplinski@gmail.com>
2019-05-22 13:21:05 -0500

    fix the CSS navbar: - add innernavbar class - add label element

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,37 +8,40 @@ const NavBar = () => {
 
   return (
     <div className="navbar">
-      <img className="nav-image" src={"favicon.png"} alt="SYB Logo" />
-      <label for="checkbox" class="ham-toggle" >🍔</label>
-      <input type="checkbox" class="hamburger-menu" id="checkbox" />
-      <li className="navbar-menu">
-        <ul className="navbar-link 1">
-          <Link to="/" className="navlinks"> Home </Link>
-        </ul>
-        <ul className="navbar-link 2">
-          <Link to="/about" className="navlinks"> About </Link>
-        </ul>
-        <ul className="navbar-link 3">
-          <Link to="/workouts" className="navlinks"> Workouts </Link>
-        </ul>
-        <ul className="navbar-link 4">
-          <Link to="/instructors" className="navlinks"> Instructors </Link>
-        </ul>
-        {client ? (
-          <ul className="navbar-link 5">
-            <Link to="/dashboard" className="navlinks"> Dashboard </Link>
+      <div className="navbar-inner">
+        <img className="nav-image" src={"favicon.png"} alt="SYB Logo" />
+        <label htmlFor="checkbox" className="ham-toggle" >🍔</label>
+        <input type="checkbox" className="hamburger-menu" id="checkbox" />
+        <li className="navbar-menu">
+          <ul className="navbar-link 1">
+            <Link to="/" className="navlinks"> Home </Link>
           </ul>
-        ) : (
-            <Fragment>
-              <ul className="navbar-link 6">
-                <Link to="/login" className="navlinks"> Login </Link>
-              </ul>
-              <ul className="navbar-link 7">
-                <Link to="/signup" className="navlinks"> Sign Up </Link>
-              </ul>
-            </Fragment>
-          )}
-      </li>
+          <ul className="navbar-link 2">
+            <Link to="/about" className="navlinks"> About </Link>
+          </ul>
+          <ul className="navbar-link 3">
+            <Link to="/workouts" className="navlinks"> Workouts </Link>
+          </ul>
+          <ul className="navbar-link 4">
+            <Link to="/instructors" className="navlinks"> Instructors </Link>
+          </ul>
+          {client ? (
+            <ul className="navbar-link 5">
+              <Link to="/dashboard" className="navlinks"> Dashboard </Link>
+            </ul>
+          ) : (
+              <Fragment>
+                <ul className="navbar-link 6">
+                  <Link to="/login" className="navlinks"> Login </Link>
+                </ul>
+                <ul className="navbar-link 7">
+                  <Link to="/signup" className="navlinks"> Sign Up </Link>
+                </ul>
+              </Fragment>
+            )}
+        </li>
+      </div>
+
     </div>
   );
 };
